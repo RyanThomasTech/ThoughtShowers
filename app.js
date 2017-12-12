35a9c0d5b37f2770e00b8389cf0062a2d75a05a8
Ryan Thomas <narraturgy@gmail.com>
2017-12-12 06:52:17 -0500

    login system fix failed redirect preventing access to main page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var exam = require('./routes/exam');
+var main = require('./routes/main');
 
 
 var app = express();
