089d168f03b15ae58feb918b79132c8be65ff609
Ryan Thomas <narraturgy@gmail.com>
2017-12-21 03:00:30 -0500

    fixed missing sql query in reading threads on userpage

--- routes/main.js
+++ routes/main.js
@@ -76,7 +76,7 @@ router.get('/newThread',function(req, res, next) {
 
 router.post('/newThread',function(req, res, next) {
   var authorID = getAuthorID(req.user.username);
-  client.query('INSERT INTO post(topic, user_account_id) VALUES($1,$2)', [req.body.topic, authorID], function(err, result) {
+  client.query('INSERT INTO thread(topic, user_account_id) VALUES($1,$2)', [req.body.topic, authorID], function(err, result) {
     if (err) {
       console.log("unable to query INSERT");
       next(err);
